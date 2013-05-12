57e98430cc3ca763b148acbed2323b56fb727214
Akhil Lodha <lodhaakhil85@gmail.com>
2013-05-12 13:42:24 -0400

    lasso regression

--- R/Data Analysis/variablereduction.R
+++ R/Data Analysis/variablereduction.R
@@ -1,5 +1,7 @@
 # This file shows how to do variable selection using the lasso method in glmnet
 # you require the R package glmnet 
+# reference http://stats.stackexchange.com/questions/34859/how-to-present-results-of-a-lasso-using-glmnet
 library('glmnet')
 model=cv.glmnet(x,y,alpha=1,nfolds=20)
 model.coef=coef(model)
+model.coef.nonZero=which(model.coef!=0)
